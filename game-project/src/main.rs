7882d753890fdcc8aaeabc56d8451bc50b10ccec
JRosSx91 <hazee.1491@gmail.com>
2023-06-02 18:10:41 +0200

    add little fn

--- game-project/src/main.rs
+++ game-project/src/main.rs
@@ -2,5 +2,14 @@ extern crate piston_window;
 use piston_window::*;
 
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |_c, g, _| {
+            clear([1.0; 4], g);
+        });
+    }
 }
